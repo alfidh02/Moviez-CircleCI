d229766793bb28be44341871cd992dc3663f5383
alfidh02 <alfidarmawan79@gmail.com>
2021-06-29 17:32:28 +0700

    remove unused import and fun

--- core/src/main/java/com/alfidh/moviez/core/domain/usecase/MovieInteractor.kt
+++ core/src/main/java/com/alfidh/moviez/core/domain/usecase/MovieInteractor.kt
@@ -2,7 +2,6 @@ package com.alfidh.moviez.core.domain.usecase
 
 import com.alfidh.moviez.core.domain.model.Movie
 import com.alfidh.moviez.core.domain.repository.IMovieRepository
-import kotlinx.coroutines.flow.Flow
 
 class MovieInteractor(private val movieRepository: IMovieRepository) : MovieUseCase {
     override fun getAllMovie() = movieRepository.getAllMovie()
