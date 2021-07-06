9f264850536c83beeb9224dda2a54cc887f35fa6
alfidh02 <alfidarmawan79@gmail.com>
2021-07-06 17:26:44 +0700

    add sqlcipher for encryption and certificate pinning

--- core/src/main/java/com/alfidh/moviez/core/di/CoreModule.kt
+++ core/src/main/java/com/alfidh/moviez/core/di/CoreModule.kt
@@ -8,6 +8,9 @@ import com.alfidh.moviez.core.data.source.remote.RemoteDataSource
 import com.alfidh.moviez.core.data.source.remote.network.ApiService
 import com.alfidh.moviez.core.domain.repository.IMovieRepository
 import com.alfidh.moviez.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,19 +22,29 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<MovieDatabase>().movieDao() }
     single {
+            val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+            val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
-            MovieDatabase::class.java, "Movie.db"
-        ).fallbackToDestructiveMigration().build()
+            MovieDatabase::class.java, "Moviez.db"
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0= ")
+            .build()
+
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
